18b72f8506a444d5002a661a898783cd2865765d
peterhcullen <45401299+peterhcullen@users.noreply.github.com>
2018-11-28 09:04:25 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDLcc8JUq9I6VKgmLZSxm8+7niIJp/9Xi64386ZY4/0Y7wWWq7UqiWXAtVE+2GGhaM77cARFEvC94zhzIdIliTvYS716y4eEZrvsT0PmT1r6fsqISb6uuD3sU6N562Bx4XCK95PFEgetyAbFf5aVd75mSrIzAcqO5r6sPk9WqNA0fAuaD8JAx0LGdes295azLhLahem2JyRsWu7WWRkpicxnQrPYV+uULYnIzO2eKGCpnMLfiktP2h5MKrD+WYXkR6oVKGSpztcrBDE/sJgUxmTCa+jq0vEPmB+pyQEd4KGCcvvEqRmZArenVs6zBueMDloFQMGAibL5/Upvj6Hh97b',
 	}  
 }
