6f9435cdfcfb88ec45effa84cbf7eb6b049efe3d
peterhcullen <45401299+peterhcullen@users.noreply.github.com>
2018-11-28 11:53:51 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,12 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar'
+  $install_dir = '/opt/minecraft/'
+  file {$install_dir:
      ensure => directory,
    }
   file {'/opt/minecraft/minecraft_server.jar':
      ensure => file,
-     source => 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
+     source => $url,
      before => Service['minecraft'],
    }
    package {'java':
