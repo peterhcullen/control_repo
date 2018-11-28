c1fcdf33f7805c6c15234c3c485ec2cba63e91cd
peterhcullen <45401299+peterhcullen@users.noreply.github.com>
2018-11-28 12:10:48 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -20,9 +20,8 @@ class minecraft (
    file {'/etc/systemd/system/minecraft.service':
      ensure => file,
      content => epp('minecraft/minecraft.service',{
-      install_dir => $install_dir
+      install_dir => $install_dir,
      })
-     source => 'puppet:///modules/minecraft/minecraft.service',
    }
    service {'minecraft':
     ensure => running,
